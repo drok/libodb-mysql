41da9891fb1092801042c9ac8b0e63765a0f993f
Boris Kolpackov <boris@codesynthesis.com>
2010-05-06 16:48:21 +0200

    Add version files

---
+++ odb/mysql/version.hxx
@@ -0,0 +1,40 @@
+// file      : odb/mysql/version.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2005-2010 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_MYSQL_VERSION_HXX
+#define ODB_MYSQL_VERSION_HXX
+
+#include <odb/version.hxx>
+
+// Version format is AABBCCDD where
+//
+// AA - major version number
+// BB - minor version number
+// CC - bugfix version number
+// DD - alpha / beta (DD + 50) version number
+//
+// When DD is not 00, 1 is subtracted from AABBCC. For example:
+//
+// Version     AABBCCDD
+// 2.0.0       02000000
+// 2.1.0       02010000
+// 2.1.1       02010100
+// 2.2.0.a1    02019901
+// 3.0.0.b2    02999952
+//
+
+// Check that we have compatible ODB version.
+//
+#if ODB_VERSION != 9901
+#  error incompatible odb interface version detected
+#endif
+
+// libodb-mysql version: odb interface version plus the bugfix
+// version.
+//
+#define LIBODB_MYSQL_VERSION     999901
+#define LIBODB_MYSQL_VERSION_STR "1.0.0.a1"
+
+#endif // ODB_MYSQL_VERSION_HXX
