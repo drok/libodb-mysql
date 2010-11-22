62e4aebe87e7dc19d66ed01fba2ea97bcc5846ad
Boris Kolpackov <boris@codesynthesis.com>
2010-11-22 12:08:52 +0200

    Use typeinfo comparator from libodb

--- odb/mysql/statement-cache.hxx
+++ odb/mysql/statement-cache.hxx
@@ -17,6 +17,7 @@
 #include <odb/mysql/object-statements.hxx>
 
 #include <odb/details/shared-ptr.hxx>
+#include <odb/details/type-info.hxx>
 
 #include <odb/mysql/details/export.hxx>
 
@@ -25,23 +26,6 @@ namespace odb
   namespace mysql
   {
     class connection;
-
-    struct LIBODB_MYSQL_EXPORT type_info_comparator
-    {
-      bool
-      operator() (const std::type_info* x, const std::type_info* y) const
-      {
-        // XL C++ on AIX has buggy type_info::before() in that
-        // it returns true for two different type_info objects
-        // that happened to be for the same type.
-        //
-#if defined(__xlC__) && defined(_AIX)
-        return *x != *y && x->before (*y);
-#else
-        return x->before (*y);
-#endif
-      }
-    };
 
     class LIBODB_MYSQL_EXPORT statement_cache
     {
@@ -70,7 +54,7 @@ namespace odb
     private:
       typedef std::map<const std::type_info*,
                        details::shared_ptr<object_statements_base>,
-                       type_info_comparator> map;
+                       details::type_info_comparator> map;
 
       connection& conn_;
       map map_;
