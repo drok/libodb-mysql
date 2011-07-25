ed22a75d665f030dcfcfc9b52764ce0e991c9169
Boris Kolpackov <boris@codesynthesis.com>
2011-07-25 09:25:47 +0200

    Bump version to 1.5.0

--- odb/mysql/version.hxx
+++ odb/mysql/version.hxx
@@ -37,7 +37,7 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 10400
+#if ODB_VERSION != 10500
 #  error incompatible odb interface version detected
 #endif
 
@@ -50,8 +50,8 @@
 // libodb-mysql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MYSQL_VERSION     1040000
-#define LIBODB_MYSQL_VERSION_STR "1.4.0"
+#define LIBODB_MYSQL_VERSION     1050000
+#define LIBODB_MYSQL_VERSION_STR "1.5.0"
 
 #include <odb/post.hxx>
 
