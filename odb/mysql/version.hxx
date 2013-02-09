e431341d9a1a5775ff6c4cccaee2483c831d02ab
Boris Kolpackov <boris@codesynthesis.com>
2013-02-09 10:33:13 +0200

    Bump version to 2.2.0

--- odb/mysql/version.hxx
+++ odb/mysql/version.hxx
@@ -36,7 +36,7 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20103
+#if ODB_VERSION != 20200
 #  error incompatible odb interface version detected
 #endif
 
@@ -49,8 +49,8 @@
 // libodb-mysql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MYSQL_VERSION     2019903
-#define LIBODB_MYSQL_VERSION_STR "2.2.0.a3"
+#define LIBODB_MYSQL_VERSION     2020000
+#define LIBODB_MYSQL_VERSION_STR "2.2.0"
 
 #include <odb/post.hxx>
 
