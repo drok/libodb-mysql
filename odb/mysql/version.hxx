602f6e07e95a53dcfb9f4a0ff79447dcac4fe5ff
Boris Kolpackov <boris@codesynthesis.com>
2017-05-24 13:47:37 +0200

    Bump version to 2.5.0-b.4.z, master is open for business

--- odb/mysql/version.hxx
+++ odb/mysql/version.hxx
@@ -36,7 +36,7 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20453
+#if ODB_VERSION != 20454
 #  error incompatible odb interface version detected
 #endif
 
@@ -49,8 +49,8 @@
 // libodb-mysql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MYSQL_VERSION     2049953
-#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.3"
+#define LIBODB_MYSQL_VERSION     2049954
+#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.4"
 
 #include <odb/post.hxx>
 
