f78081faeab32e8a8b84e2c952c5b59a56791922
Boris Kolpackov <boris@codesynthesis.com>
2019-02-19 07:44:57 +0200

    Release version 2.5.0-b.11

--- odb/mysql/version.hxx
+++ odb/mysql/version.hxx
@@ -40,7 +40,7 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20460
+#if ODB_VERSION != 20461
 #  error incompatible odb interface version detected
 #endif
 
@@ -53,8 +53,8 @@
 // libodb-mysql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MYSQL_VERSION     2049960
-#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.10"
+#define LIBODB_MYSQL_VERSION     2049961
+#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.11"
 
 #include <odb/post.hxx>
 
