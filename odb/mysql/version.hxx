c2795b4cb43d8ed2a2e3857501ee46c48605f791
Boris Kolpackov <boris@codesynthesis.com>
2022-07-26 08:47:24 +0200

    Release version 2.5.0-b.23

--- odb/mysql/version.hxx
+++ odb/mysql/version.hxx
@@ -39,7 +39,7 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20472
+#if ODB_VERSION != 20473
 #  error incompatible odb interface version detected
 #endif
 
@@ -52,8 +52,8 @@
 // libodb-mysql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_MYSQL_VERSION     2049972
-#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.22"
+#define LIBODB_MYSQL_VERSION     2049973
+#define LIBODB_MYSQL_VERSION_STR "2.5.0-b.23"
 
 #include <odb/post.hxx>
 
